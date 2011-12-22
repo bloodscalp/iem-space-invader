41403c908c4b29d0c9fe00f24c86a2dcbdcbed0e
Romain Failletaz <romain.failletaz@heig-vd.ch>
2011-12-22 12:35:47 +0100

    Driver compile mais n'as pas ete teste.

--- rt-app/i2ctest.c
+++ rt-app/i2ctest.c
@@ -6,6 +6,15 @@
 int main(int argc, char **argv) {
 
 
+	mknod("/var/dev/i2c", S_IFCHR|S_IRUSR|S_IWUSR|S_IRGRP|S_IWGRP|S_IROTH|S_IWOTH, makedev(89,0));
+
+	int i2c_fd = open("/var/dev/i2c", O_RDWR);
+
+	char buf[2];
+	while(1) {
+		read(i2c_fd, buf, 2);
+		printf("Lecture %X",(int)buf);
+	}
 
 	return 0;
 
--- rt-app/pca9554-m.c
+++ rt-app/pca9554-m.c
@@ -19,8 +19,11 @@
 
 #define I2C_SLAVE 0x0703 	/* IOCTL CMD value to be passed to xeno_i2c_ioctl */
 
-#define I2C_IO_MODES 0xF0;
+#define I2C_IO_MODES 0xF0
 
+#define BUFF_SIZE 2
+
+struct cdev *i2c_dev;
 
 typedef enum i2c_commands_t {
 	INPUT,
@@ -30,17 +33,15 @@ typedef enum i2c_commands_t {
 } i2c_commands_t;
 
 struct file_operations fops = {
-		// FIXME bizarre ici, il veut pas .close
-		//.close 	= pca9554_close,
-		.open 	= pca9554_open,
-		//pca9554_ioctl
-		.read 	= pca9554_read,
-		.write	= pca9554_write,
+	//.close 	= pca9554_close,
+	//.open 	= pca9554_open,
+	.read 	= pca9554_read,
+	.write	= pca9554_write,
+	.ioctl 	= pca9554_ioctl
 };
 
+
 int pca9554_open(struct inode *inode, struct file *file) {
-
-
 
 	return 0;
 }
@@ -57,35 +58,51 @@ int pca9554_ioctl(struct inode * inode, struct file *file, unsigned int cmd, uns
 
 int pca9554_close(struct inode *inode, struct file *file) {
 
-	/* A compléter ... */
 
 	return 0;
 }
 
 ssize_t pca9554_read(struct file *file, char __user *buff, size_t len, loff_t *off) {
 	int err;
-	int i;
-	char fileBuff[2*len+1];
+	char kbuf[len];
 
-	if( (err = xeno_i2c_read(buff, len)) < 0) {
-		printk("I2C read error : %d", err);
+	kbuf[0] = INPUT;
+
+	if( (err = xeno_i2c_write(kbuf, 1)) < 0) {
+		printk("I2C read config error : %d", err);
 		return err;
 	}
 
-	for(i=0; i<2*len+1; i+=2){
-		fileBuff[i] = buff[i];
-		fileBuff[i+1] = '\n';
+	if ( (err = xeno_i2c_read(kbuf, len)) < 0) {
+		printk("I2C read error : %d", err);
+		return err;
+	} else {
+		if( (err = copy_to_user(buff, kbuf, len)) > 0 ) {
+			printk("copy to user error : %d B not copied", err);
+			return -1;
+		}
 	}
-	fileBuff[i] = 0x05;
-
-	fwrite(file, fileBuff);
 
 	return 0;
 }
 
 ssize_t pca9554_write(struct file *file, const char __user *buff, size_t len, loff_t *off) {
 	int err;
-	if( (err = xeno_i2c_write(buff, len)) < 0) {
+	char kbuf[len];
+
+	kbuf[0] = OUTPUT;
+
+	if( (err = xeno_i2c_write(kbuf, 1)) < 0) {
+		printk("I2C write config error : %d", err);
+		return err;
+	}
+
+	if( (err = copy_from_user(kbuf, buff, len)) > 0 ) {
+		printk("copy from user error : %d B not copied", err);
+		return -1;
+	}
+
+	if( (err = xeno_i2c_write(kbuf, len)) < 0) {
 		printk("I2C write error : %d", err);
 		return err;
 	}
@@ -93,25 +110,44 @@ ssize_t pca9554_write(struct file *file, const char __user *buff, size_t len, lo
 	return 0;
 }
 
+/**
+ *  major 89  I2C bus interface
+ *		  0 = /dev/i2c-0	First I2C adapter
+ *		  1 = /dev/i2c-1	Second I2C adapter
+ *
+ */
 int __init init_module(void) {
+
 	int err;
+
+	char buff[BUFF_SIZE];
+
+	/* Enregistrement du driver */
+	dev_t dev;
+
+	dev = MKDEV(89, 0);
+	i2c_dev = cdev_alloc();
+	cdev_init(i2c_dev, &fops);
+
+	i2c_dev->owner = THIS_MODULE;
+	cdev_add(i2c_dev, dev, 1);
+
+
+
+	/* Initialisation du périphérique i2c */
 	if( (err = xeno_i2c_init()) < 0) {
 		printk("I2C init error : %d", err);
 		return err;
 	}
 
+
 	/* IO pins configuration */
 	xeno_i2c_ioctl(I2C_SLAVE, 0x0);
-
-	size_t len;
-	len = 2;
-
-	char buff[len];
 
 	buff[0] = CONFIG;
 	buff[1] = I2C_IO_MODES;
 
-	if( (err = xeno_i2c_write(buff, len)) < 0) {
+	if( (err = xeno_i2c_write(buff, BUFF_SIZE)) < 0) {
 		printk("I2C io modes config error : %d", err);
 		return err;
 	}
@@ -121,10 +157,16 @@ int __init init_module(void) {
 
 void __exit cleanup_module(void) {
 	int err;
+
+
+	/* Delete device file */
+	cdev_del(i2c_dev);
+
+	/* Exit i2c module */
 	if( (err = xeno_i2c_exit()) < 0) {
 		printk("I2C cleanup error : %d", err);
 	}
-
 }
+
 
 MODULE_LICENSE("GPL");
