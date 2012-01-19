0f667dec848f07114a4cf7fb31a6a507a142ddb7
Romain Failletaz <romain.failletaz@heig-vd.ch>
2012-01-19 10:35:34 +0100

    add ennemi.c .h

---
+++ rt-app/ennemi.h
@@ -0,0 +1,32 @@
+/*
+ * ennemi.h
+ *
+ *  Created on: 19 janv. 2012
+ *      Author: Romain Failletaz
+ */
+
+#ifndef ENNEMI_H_
+#define ENNEMI_H_
+
+#define nbEnnemis 24		// doit être un multiple du nombre de vagues ennemies
+#define nbVagueEnnemis 4
+#define DIRECTION_EST 1
+#define DIRECTION_OUEST -1
+#define SHIT_SIZE 16
+#define DEFAULT_PV_ENNEMI 20
+#define X_SPACE 4
+#define Y_SPACE 4
+#define xStart 10
+#define yStart 10
+
+extern RT_TASK ennemi_task;
+extern RT_MUTEX mutex_ennemi;
+
+extern t_ennemi_ ennemi[nbEnnemis];
+
+extern int ennemi_init(void);
+extern void move_ennemi(void* cookie);
+bool detectShitEnable(void);
+void show_ennemi(void);
+
+#endif /* ENNEMI_H_ */
